7248f96e046a0fc9d081406137cc81278ea5b75e
Ahmad Idrees <ahmadidrees1197@gmail.com>
2022-07-18 09:38:23 +0500

    Changes in FirebaseFCM class

--- firebaseLib/src/main/java/ai/bom/firebase/lib/fcm/FirebaseFCM.kt
+++ firebaseLib/src/main/java/ai/bom/firebase/lib/fcm/FirebaseFCM.kt
@@ -9,35 +9,36 @@ import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 
-object FirebaseFCM {
-
-    fun initializeFCM(context: Context, topic: String) {
-        try {
-            FirebaseApp.initializeApp(context)
-        } catch (e: Exception) {
-            Log.i(TAG, "onCreate: ${e.message}")
+class FirebaseFCM {
+    companion object {
+        fun initializeFCM(context: Context, topic: String) {
+            try {
+                FirebaseApp.initializeApp(context)
+            } catch (e: Exception) {
+                Log.i(TAG, "onCreate: ${e.message}")
+            }
+            createNotificationChannel(context)
+            //Subscribe To Topic
+            FirebaseMessaging.getInstance().subscribeToTopic(topic)
         }
-        createNotificationChannel(context)
-        //Subscribe To Topic
-        FirebaseMessaging.getInstance().subscribeToTopic(topic)
-    }
 
-    fun removeFCMTopic(topic: String) {
-        FirebaseMessaging.getInstance().unsubscribeFromTopic(topic)
-    }
+        fun removeFCMTopic(topic: String) {
+            FirebaseMessaging.getInstance().unsubscribeFromTopic(topic)
+        }
 
-    private fun createNotificationChannel(context: Context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = context.getString(R.string.default_notification_channel_id)
-            val channelName = context.getString(R.string.default_notification_channel_name)
-            val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(
-                NotificationChannel(
-                    channelId,
-                    channelName, NotificationManager.IMPORTANCE_DEFAULT
+        private fun createNotificationChannel(context: Context) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channelId = context.getString(R.string.default_notification_channel_id)
+                val channelName = context.getString(R.string.default_notification_channel_name)
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(
+                    NotificationChannel(
+                        channelId,
+                        channelName, NotificationManager.IMPORTANCE_DEFAULT
+                    )
                 )
-            )
+            }
         }
     }
 }
